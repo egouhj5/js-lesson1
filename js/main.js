2a1ee27ebb035fb970a2f7dec2bb38712b944ffd
egouhj5 <kn09032010@gmail.com>
2024-07-30 15:25:45 +0300

    add js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let age = 14;
+console.log(age);
+let firstName = "Nikita";
+console.log(firstName);
+let = isStudent = true;
+console.log(isStudent);
+const myString = "я заперщаю вам срать 🐊" 
+console.log(myString);
+let myNumber = 993;
+myNumber += 10;
+console.log(myNumber);
+const myNull = null;
+console.log(myNull);
+alert("У тебе є можливість вввести дату смерті старого бога!!!!!!!!");
+let DateOfDeathOfStariyBog = prompt("Введіть дату:");
+alert(`Чорт, старий бог помре в: ${DateOfDeathOfStariyBog}`);
+let text;
+if (confirm(`Ви точно впевнені що старий бог помре в ${DateOfDeathOfStariyBog}`) == true) {
+    text = "ЛЕсс Гоу!!";
+} else {
+    text = `Ураа старий бог не помре в ${DateOfDeathOfStariyBog}!!`;
+}
+alert(text);
